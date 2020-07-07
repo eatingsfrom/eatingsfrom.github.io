65c3c191ce47a836bc95ebab9c5296821a6ae325
eatingsfrom <eatingsfrom@gmail.com>
2020-07-07 11:02:39 -0400

    initial release 🍴

---
+++ precache-manifest.ea00bd9db2760cd79ea27b1e72c204ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8c7b66d47b022d1c737fcada11f72d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebb80a1b9cb8ddeb2c7c",
+    "url": "/static/css/main.5f0a366f.chunk.css"
+  },
+  {
+    "revision": "cf56175f920527d4b152",
+    "url": "/static/js/2.f0a0b8ac.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.f0a0b8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb80a1b9cb8ddeb2c7c",
+    "url": "/static/js/main.7abe9723.chunk.js"
+  },
+  {
+    "revision": "d3f5edd9ba5893a4bf06",
+    "url": "/static/js/runtime-main.79284d27.js"
+  },
+  {
+    "revision": "de7871ed24e7bcc0615f44ecbd9d2c2a",
+    "url": "/static/media/about-us.de7871ed.jpg"
+  }
+]);
